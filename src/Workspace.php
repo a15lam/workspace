d66b469b84b1a145b167fe92014dea8a30a8d800
Arif Islam <arif@dreamfactory.com>
2016-08-28 14:18:06 -0400

    updates

--- src/Workspace.php
+++ src/Workspace.php
@@ -7,24 +7,41 @@ use a15lam\Workspace\Utility\Logger;
 
 class Workspace
 {
+    /** @var Config|null  */
     protected static $config = null;
 
+    /** @var Logger|null  */
     protected static $logger = null;
 
+    /** @var string  */
+    protected static $configInfo = __DIR__ . '/../config.php';
+
+    /** @var string  */
+    protected static $logPath = __DIR__ . '/../storage/logs/';
+
+    /**
+     * Sets the config class
+     */
     protected static function setConfig()
     {
-        static::$config = new Config(__DIR__ . '/../config.php');
+        static::$config = new Config(static::$configInfo);
     }
 
+    /**
+     * Sets the logger class
+     */
     protected static function setLogger()
     {
         static::$logger = new Logger(
-            __DIR__ . '/../storage/logs/',
+            static::$logPath,
             static::config()->get('log_level', Logger::WARNING),
             static::config()->get('timezone')
         );
     }
 
+    /**
+     * @return \a15lam\Workspace\Utility\Config
+     */
     public static function config()
     {
         if (static::$config === null) {
@@ -34,6 +51,9 @@ class Workspace
         return static::$config;
     }
 
+    /**
+     * @return \a15lam\Workspace\Utility\Logger
+     */
     public static function log()
     {
         if (static::$logger === null) {
